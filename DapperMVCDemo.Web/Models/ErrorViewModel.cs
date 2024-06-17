f658a0a9ea023e2663f091cbbcdf2898764ec0c2
“DEBORAJ <deborajroy123@gmail.com>
2024-06-17 23:03:35 +0600

    Add project files.

---
+++ DapperMVCDemo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DapperMVCDemo.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
